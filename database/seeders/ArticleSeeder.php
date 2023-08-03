a8b2c7abd66091e77c3fb944cf5ecacada3b2301
kaanpasa <kaanpasa@protonmail.com>
2023-08-03 23:46:39 +0300

    Added ArticleSeeder and articles table

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+use DB;
+require_once 'vendor/autoload.php';
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+     
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for($i=0; $i<4; $i++){
+            $title=$faker->sentence(6);
+            DB::table('articles')->insert([
+                'categoryId'=>rand(1,7),
+                'title'=>$title,
+                'image'=>$faker->imageUrl(800, 400, 'Kaan Pasa'),
+                'content'=>$faker->paragraph(6),
+                'slug'=>Str::slug($title),
+                'created_at'=>now(),
+                'updated_at'=>now()
+            ]);
+        }
+    }
+}
