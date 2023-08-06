8bc11475f3b4437b728b533dcf7cee7338ed7a43
kaanpasa <kaanpasa@protonmail.com>
2023-08-06 16:18:08 +0300

    Added admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Backend Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+*/
+
+Route::get('admin/panel', 'App\Http\Controllers\Back\Dashboard@index')->name('admin.dashboard');
+Route::get('admin/giris', 'App\Http\Controllers\Back\Auth@login')->name('admin.login');
+
+
+/*
+|--------------------------------------------------------------------------
+| Frontend Routes
+|--------------------------------------------------------------------------
 */
 
 Route::get('/', 'App\Http\Controllers\Front\Homepage@index')->name('homepage');
