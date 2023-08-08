ab19919bebe47a4c5a839274e664d4ab8d0f7369
kaanpasa <kaanpasa@protonmail.com>
2023-08-08 19:23:57 +0300

    Added middleware for admin auth

--- resources/views/back/layouts/footer.blade.php
+++ resources/views/back/layouts/footer.blade.php
@@ -30,15 +30,15 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Çıkış Yapmak İstediğinize Emin Misiniz?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Oturumu sonlandırmak için "Çıkış Yap" seçeneğine tıklayın.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Hayır</button>
+                    <a class="btn btn-primary" href="{{route('admin.logout')}}">Çıkış Yap</a>
                 </div>
             </div>
         </div>
